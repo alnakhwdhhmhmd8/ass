0a7e7e6b6e606e785f3928f1f8ed6fa0768c1021
alnakhwdhhmhmd8 <alnakhwdhhmhmd8@gmail.com>
2025-09-24 20:06:05 +0300

    Uploaded Successfully

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["wwvvwl","wwvvwl"]
+OWNER_NAME = "√ َِsoltan"
+BOT_TOKEN = "7604085402:AAF3rR5GeB2hCLbBs0VmPQcTiOLHPWPXy6Y"
+DATABASE = "mongodb+srv://islam2005:islam2005@cluster0.rdjot.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/xvwwvl"
+GROUP = "https://t.me/xvwwvl"
+VIDEO = "https://t.me/WMWBKL/16"
+LOGS = "xvwwvl"
+
